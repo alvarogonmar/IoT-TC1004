3a005e169bd6469c8ab1c621a46b0262683b3d76
alvarogonmar <alvarogonmar0@gmail.com>
2025-12-04 18:33:17 -0600

    Add send_post_request function definition in simuladorRaspberry.py

--- simuladorRaspberry.py
+++ simuladorRaspberry.py
@@ -7,3 +7,7 @@ import json
 TARGET_URL = "https://sensor-api-rho.vercel.app/sensor"  # Replace with your actual API endpoint
 SLEEP_INTERVAL_SECONDS = 5 * 60  # 5 minutes in seconds
 SENSOR_IDS = [1, 2, 3]
+
+ 
+
+def send_post_request(url, sensor_id):

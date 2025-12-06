5a468bae9b24058d40f811ad39b910f773215644
alvarogonmar <alvarogonmar0@gmail.com>
2025-12-06 12:06:35 -0600

    Add sensor iteration in POST request loop in simuladorRaspberry.py

--- simuladorRaspberry.py
+++ simuladorRaspberry.py
@@ -25,3 +25,4 @@ def send_post_request(url, sensor_id):
 if __name__ == "__main__":
     print(f"Starting POST request loop to {TARGET_URL} with a {SLEEP_INTERVAL_SECONDS/60}-minute interval.")
     while True:
+        for sensor in SENSOR_IDS:
